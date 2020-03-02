26504471cf342b3cfc742c9296b9e8b77c8edc82
Arthur FOUQUET <afouquet@localhost.localdomain>
2020-03-02 12:10:17 +0100

    ex3 v2

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 
+const PORT = process.env.PORT || 8080;
+const app = express();
+
 function startServeur() {
-  const PORT = process.env.PORT || 8080;
-  const app = express();
-  app.get('/hello', (request, response) => response.send('world'))
+  app.get('/hello', (req, res) => {
+    res.send('world');
+  });
   app.listen(PORT);
 }
 
