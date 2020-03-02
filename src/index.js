4d61e0621eda16b3ea2bb185c89fd6719a494191
Arthur FOUQUET <afouquet@localhost.localdomain>
2020-03-02 18:18:16 +0100

    preque fini ex 4

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ const PORT = process.env.PORT || 8080;
 const app = express();
 const bodyParser = require('body-parser');
 
+const cookieParser = require('cookie-parser');
+
+app.use(cookieParser());
+
 app.use(bodyParser.json());
 
 function startServeur() {
@@ -12,6 +16,7 @@ function startServeur() {
   });
   app.get('/repeat-my-fixed', (req, res) => {
     res.send('For better and for worst\n'.repeat(200));
+    return (200);
   });
   app.get('/repeat-my-query', (req, res) => {
     const { message } = req.query;
@@ -31,7 +36,27 @@ function startServeur() {
       res.status = 400;
     }
   });
+  app.get('/repeat-my-header', (req, res) => {
+    const msg = req.headers;
+    const msg2 = Object.keys(msg);
+    const nbr = msg2.indexOf('x-message');
+    if (nbr !== -1) {
+      const oui = Object.values(msg);
+      res.send(oui[nbr]);
+    } else {
+      res.send('Bad Request');
+      res.status = 400;
+    }
+  });
+  app.get('/repeat-my-cookie', (req, res) => {
+    const request = req.cookies;
+    if (request !== null && Object.keys(request).includes('message') !== false) {
+      res.send(request.message);
+    } else {
+      res.send('Bad Request');
+      res.status = 400;
+    }
+  });
   app.listen(PORT);
 }
-
 startServeur();
